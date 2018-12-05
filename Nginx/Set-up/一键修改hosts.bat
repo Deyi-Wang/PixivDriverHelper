c248a41e9b13872a280ae5dcece0b95b6ea9536b
守望时空33 <yanhanhui2001a@163.com>
2018-12-05 12:28:47 +0800

    v1.0

--- Nginx/Set-up/一键修改hosts.bat
+++ Nginx/Set-up/一键修改hosts.bat
@@ -2,6 +2,6 @@
 echo 是否修改hosts？修改前建议先备份原hosts文件。
 echo 若修改失败，请先关闭所有杀毒软件并右键以管理员身份运行此程序！
 pause
-copy /y %~dp0hosts C:\Windows\System32\drivers\etc\hosts
+copy /y "%~dp0hosts" "C:\Windows\System32\drivers\etc\"
 echo 修改完成，按任意键退出！
 pause >nul
